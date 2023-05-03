51938c80f007366740bd59b4417afa9bd0217c5e
HoangDIeu <Thaihoangdieu99@gmail.com>
2023-05-03 17:54:40 +0700

    link

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Meta from '../components/Meta';
 import BreadCrumb from '../components/BreadCrumb';
-import { Link } from 'react-router-dom';
 
 const ResetPassword = () => {
   return (
